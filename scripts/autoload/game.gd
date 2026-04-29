d65e5ce8049961188e748e8097c0ee02a3417f24
visperc <visperc@gmail.com>
2026-04-29 13:53:34 +0800

    Fix Godot project entry files

---
+++ scripts/autoload/game.gd
@@ -0,0 +1,6 @@
+extends Node
+
+var content_version: String = "builtin"
+
+func set_content_version(value: String) -> void:
+	content_version = value
---
+++ scripts/autoload/main_scene.gd
@@ -0,0 +1,4 @@
+extends Node
+
+func _ready() -> void:
+	print("Balatro logic prototype loaded. Use C# GameBootstrap for runtime integration.")
