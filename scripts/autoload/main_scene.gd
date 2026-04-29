e50986ff3f50ea05cd1c964ea58edf290e462a47
visperc <visperc@gmail.com>
2026-04-29 21:04:18 +0800

    Remove C# hard dependency from startup scene

--- scripts/autoload/main_scene.gd
+++ scripts/autoload/main_scene.gd
@@ -1,4 +1,4 @@
 extends Node
 
 func _ready() -> void:
-	print("Balatro logic prototype loaded. Use C# GameBootstrap for runtime integration.")
+	print("Balatro logic prototype loaded. C# runtime builds successfully; attach GameBootstrap after Godot imports C# scripts.")
